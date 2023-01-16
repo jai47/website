7d7987153117ae6540d6f354e63238ac77ea9d9f
Jai <official.jaimishra@gmail.com>
2023-01-16 22:47:16 +0530

    main commit

--- script/home.js
+++ script/home.js
@@ -18,14 +18,14 @@ for (n=23;n>=0;n--){
     let div = document.createElement("div");
     let i = document.createElement("i");
     let innerDiv = document.createElement("div");
-    let h1 = document.createElement("h1");
+    // let h1 = document.createElement("h1");
     div.setAttribute("class","moreOptions");
     div.setAttribute("id",`20${j}`);
     i.setAttribute("class","bx bx-x");
     i.setAttribute("onclick",`showList('20${j}')`);
     innerDiv.setAttribute("class","container scrolling");
-    h1.innerText=`20${j}`;
-    innerDiv.appendChild(h1)
+    // h1.innerText=`20${j}`;
+    // innerDiv.appendChild(h1)
     div.appendChild(i);
     div.appendChild(innerDiv);
     folder.appendChild(div);    
@@ -37,4 +37,50 @@ function showOptions() {
 
 function showList(e){
     document.getElementById(e).classList.toggle('moreOptionsActive');
+}
+
+var xhReq = new XMLHttpRequest();
+xhReq.open("GET", '/script/data.json', false);
+xhReq.send(null);
+
+var jsonObject = JSON.parse(xhReq.responseText);
+ 
+
+let people = {
+    data : jsonObject
+}
+
+
+for (let i of people.data){
+    let card =  document.createElement("div");
+    card.classList.add("card");
+    let imgB =  document.createElement("div");
+    imgB.classList.add("imgBx");
+    let img = document.createElement("img");
+    img.setAttribute("src","location.png")
+    img.setAttribute("alt",`${i.F_NAME}`);
+    let contentDiv = document.createElement("div");
+    contentDiv.classList.add("content");
+    let detailDiv = document.createElement("div");
+    detailDiv.classList.add("details");
+    let h2 = document.createElement("h2");
+    h2.innerText = `${i.title} ${i.F_NAME} ${i.L_NAME}\n`;
+    let span = document.createElement("span");
+    span.innerText = `${i.Designation}`
+    h2.appendChild(span);
+    let dataDiv = document.createElement("div");
+    dataDiv.classList.add("data");
+    let para = document.createElement("p");
+    para.innerText = `${i.Attending} ${i.Company} ${i.Email} ${i.DOB}`;
+    dataDiv.appendChild(para);
+    detailDiv.appendChild(h2);
+    detailDiv.appendChild(dataDiv);
+    contentDiv.appendChild(detailDiv);
+    imgB.appendChild(img);
+    card.appendChild(imgB);
+    card.appendChild(contentDiv);
+
+    document.getElementById(i.Attending).children[1].appendChild(card);
+
+
 }
